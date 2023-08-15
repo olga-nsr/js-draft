c35215b17ec66a164cb7f3540d4c2d72a3357521
Olga-alexsandrova <berduginaolga5@gmail.com>
2023-08-15 19:35:49 +0300

    scr1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// let value = 1;
+// if(value === 10) {
+//     console.log('condition true');
+// } else{
+//     console.log('condition false'); 
+// }
+
+//switch (порівнюємо тільки рядок с рядком, число з числом)
+// const name = "A";
+// switch (name) {
+//     case 'k' :
+//         console.log('Value Каte');
+//         break;
+//         case 'Ivan':
+//             console.log('Value T');
+//             break;
+//             case "A":
+//             console.log('Value A');
+//             break;
+// }
+/**Тернарный оператор */
+//condition ? true : false
+const value = 17;
+let message = value > 15 && value < 20? 'true' : 'false';
+console.log(message);
+
